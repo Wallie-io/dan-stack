5a6fcd24dacc766ebaee43bebfffde18b0dcb147
Jake <jake@xyzdigital.com>
2023-08-24 18:03:03 -0700

    remove unneeded routes

--- api/src/project/project.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { leadAuth, leadCallBack } from './project.controller'
-
-export const router = express.Router()
-
-router.get('/projects', leadAuth)
-router.get('/projects/:id', leadCallBack)
-
-export default router
--- api/src/project/services/getProject.ts
+++
@@ -1,8 +0,0 @@
-import { Project } from '../project.model'
-import { ID } from '../../utils'
-
-export const getProject = (id: ID, brand: ID) => {
-  if (!id) throw new Error('id parameter required')
-  if (!brand) throw new Error('brand parameter required')
-  Project.find({ id, brand })
-}
